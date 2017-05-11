e9cd5b2a9a800587e58291b46f3df52e4fbd3d98
zhengmingjun <keshiim@163.com>
2017-05-11 13:01:43 +0800

    Initial Commit

---
+++ ZMJImageEditorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZMJImageEditorDemo
+//
+//  Created by Jason on 2017/5/11.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZMJImageEditorDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZMJImageEditorDemo
+//
+//  Created by Jason on 2017/5/11.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
