8864a21956a8bd706d5b26e85222a75f2c838ac6
zhengmingjun <keshiim@163.com>
2017-05-11 13:25:51 +0800

    mark

--- ZMJImageEditorDemo/ViewController.m
+++ ZMJImageEditorDemo/ViewController.m
@@ -14,6 +14,7 @@
 
 @implementation ViewController
 
+#pragma mark - Life circle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
